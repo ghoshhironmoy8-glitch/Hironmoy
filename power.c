a060a39f1026bd15930950644c994f9cfb9f6e51
ghoshhironmoy8-glitch <ghoshhironmoy8@gmail.com>
2026-04-02 00:46:33 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+long long power(int base, int exp) {
+    if (exp == 0)
+        return 1;
+    return base * power(base, exp - 1);
+}
+
+int main() {
+    int base, exp;
+
+    printf("Enter base and exponent: ");
+    scanf("%d %d", &base, &exp);
+
+    printf("Result = %lld\n", power(base, exp));
+
+    return 0;
+}
